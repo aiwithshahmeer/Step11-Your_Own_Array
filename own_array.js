b24baab3f73f067c6ff8deaa4570bc4f9d085259
SHAHMEER AFTAB <154751468+aiwithshahmeer@users.noreply.github.com>
2024-03-09 01:55:35 +0500

    Add files via upload

---
+++ own_array.js
@@ -0,0 +1,5 @@
+"use strict";
+let transportation = ['Ninja H2r Bike', 'Supra mk4 Car', 'Bullet Train', 'Continental Cycle'];
+for (let i = 0; i < transportation.length; i++) {
+    console.log('I would like to own a ' + transportation[i]);
+}
---
+++ own_array.ts
@@ -0,0 +1,9 @@
+
+
+let transportation:string[] =['Ninja H2r Bike', 'Supra mk4 Car', 'Bullet Train', 'Continental Cycle'];
+
+for(let i=0; i<transportation.length; i++){
+
+    console.log('I would like to own a ' + transportation[i]);
+
+}
